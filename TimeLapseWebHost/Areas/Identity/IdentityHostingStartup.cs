f039563c6b5ce5283e20f4a09beb8a8b9ed13853
Felix Cornelissen <felixcornelissen@gmail.com>
2020-05-02 14:16:34 +0200

    scaffold

---
+++ TimeLapseWebHost/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TimeLapseWebHost.Data;
+
+[assembly: HostingStartup(typeof(TimeLapseWebHost.Areas.Identity.IdentityHostingStartup))]
+namespace TimeLapseWebHost.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
